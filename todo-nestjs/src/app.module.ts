9e4f04386f8602450468a1d9452683541c011a5e
Alexander <freddy.coronel@epn.edu.ec>
2020-01-16 20:39:51 -0500

    added the entities and conecction to the db

--- todo-nestjs/src/app.module.ts
+++ todo-nestjs/src/app.module.ts
@@ -5,13 +5,24 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { TareaModule } from './tarea/tarea.module';
 import { EstadoModule } from './estado/estado.module';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UsuarioModule,
     TareaModule,
     EstadoModule,
-    TagModule
+    TagModule,
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'mysql',
+      port: 32769,
+      username: 'root',
+      password: 'alex1995',
+      database: 'tododb',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- todo-nestjs/src/usuario/usuario.module.ts
+++ todo-nestjs/src/usuario/usuario.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsuarioController } from './usuario.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forFeature([UsuarioEntity], 'default)
+  ],
   controllers: [UsuarioController],
   providers: [],
   exports: [],
